a27a14a76eff0b4566d08920a930164b1955117f
Yasser <yasserbal@icloud.com>
2022-06-14 14:20:44 +0200

    Pokemoves now works properly for pokemon with <4 moves

--- index.php
+++ index.php
@@ -11,21 +11,22 @@
 <body>
     <?php
     if (isset($_POST['lookUpPokemon'])) {
-        // $userInput = $_POST['searchID'];
-        $userInput = '1';
+        $userInput = $_POST['searchID'];
         $API_URL = "https://pokeapi.co/api/v2/pokemon/";
         $patternToCheckForNumbers = "/^\d+$/";
-        if (preg_match($patternToCheckForNumbers, $userInput)) {
-            echo "Only Numbers inputed";
-        }
-        $patternToCheckIfFirstCharIsUpperCase = "/^[A-Z]/";
-        if (preg_match($patternToCheckIfFirstCharIsUpperCase, $userInput)) {
-            echo "First char is uppercase";
-        }
-
         $data = file_get_contents($API_URL . $userInput . '/');
         $pokemon = json_decode($data);
-        var_dump($pokemon);
+        $pokeName = $pokemon->name;
+        $pokeID = $pokemon->id;
+        $pokeMoves = [];
+        $pokeMovesLength = count($pokemon->moves);
+        for ($i = 0; $i <= 4; $i++) {
+            if ($i >= $pokeMovesLength) {
+                break;
+            }
+            $pokeMove = $pokemon->moves[$i]->move->name;
+            array_push($pokeMoves, $pokeMove);
+        }
     }
     ?>
 
@@ -34,11 +35,49 @@
     </header>
     <main>
         <form method="post">
+            <p>Please enter the ID of the pokemon you would like to look up</p>
             <input type="text" name="searchID" id="searchPokemon">
             <input type="submit" value="Look Up Pokemon" name="lookUpPokemon">
         </form>
+        <table>
+            <tr>
+                <td>Name:</td>
+                <td><?php if (isset($pokeName)) {
+                        echo $pokeName;
+                    } ?></td>
+            </tr>
+            <tr>
+                <td>ID: </td>
+                <td><?php if (isset($pokeID)) {
+                        echo $pokeID;
+                    } ?></td>
+            </tr>
+            <tr>
+                <td colspan="4">Moves</td>
+            </tr>
+            <td><?php if (isset($pokeMoves[0])) {
+                    echo $pokeMoves[0];
+                }
+                ?></td>
+            <td><?php if (isset($pokeMoves[1])) {
+                    echo $pokeMoves[1];
+                }
+                ?></td>
+            <td><?php if (isset($pokeMoves[2])) {
+                    echo $pokeMoves[2];
+                }
+                ?></td>
+            <td><?php if (isset($pokeMoves[3])) {
+                    echo $pokeMoves[3];
+                }
+                ?></td>
+            <tr>
 
+            </tr>
+        </table>
     </main>
+
+
     <footer>
         <h1>
             Footer
